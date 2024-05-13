56443456820782f0c07b2b20217e63d9bc4ae08f
Eniolaonafujabi <165755379+Eniolaonafujabi@users.noreply.github.com>
2024-05-13 21:26:07 +0100

    work

--- DaySeven/Phonecontact/java/PhoneBookDriverClass.java
+++ DaySeven/Phonecontact/java/PhoneBookDriverClass.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 public class PhoneBookDriverClass{
 	public static void main(String[] args){
-		PhoneBookFunction eniola = new PhoneBookFunction();
-		PhoneBookFunction janet = new PhoneBookFunction();
+		Scanner collector = new Scanner(System.in);
+		String phoneBookOption = """
+			1 -> Add contact
+			2 -> Remove contact
+			3 -> Find Contact By phone number
+			4 -> Find Contact By first name
+			5 -> Find Contact By last name
+			6 -> Exit  
+		""";
+		System.out.println(phoneBookOption);
+			int userinput = collector.nextInt();
+		switch(userinput){
+		case 1: addContact();
+		case 2: removeContact();
+		case 3: findContactByPhoneNumber();
+		case 4: findContactByFirstName();
+		case 5: findContactByLastName();
+		case 6: System.out.println("Exit");
+		}
 	}
 }
