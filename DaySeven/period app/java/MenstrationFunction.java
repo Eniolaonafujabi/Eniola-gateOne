9f25c88deabcd5be39b49b5e57e31124c7ec08a1
Eniolaonafujabi <165755379+Eniolaonafujabi@users.noreply.github.com>
2024-05-15 20:29:42 +0100

    work

---
+++ DaySeven/period app/java/MenstrationFunction.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+public class MenstrationFunction{
+	public static int menstration(int yearAndFirstDayOfYourLastMenstration,int monthAndFirstDayOfYourLastMenstration,int dayAndFirstDayOfYourLastMenstration,int yearAndLastDayOfYourLastMenstration,int monthAndLastDayOfYourLastMenstration,int dayAndLastDayOfYourLastMenstration,long averageCycle){
+	LocalDate lastMenstrationDayFirstDay = LocalDate.of(yearAndFirstDayOfYourLastMenstration,monthAndFirstDayOfYourLastMenstration,dayAndFirstDayOfYourLastMenstration);
+	LocalDate lastMenstrationDayLastDay = LocalDate.of(yearAndLastDayOfYourLastMenstration,monthAndLastDayOfYourLastMenstration,dayAndLastDayOfYourLastMenstration);
+	LocalDate nextPeriod = lastMenstrationDayFirstDay.plusDay(averageCycle);
+	return nextPeriod;
+	}
+
+	public static String menstration(int yearAndFirstDayOfYourLastMenstration,int monthAndFirstDayOfYourLastMenstration,int dayAndFirstDayOfYourLastMenstration,int yearAndLastDayOfYourLastMenstration,int monthAndLastDayOfYourLastMenstration,int dayAndLastDayOfYourLastMenstration,long averageCycle){
+	LocalDate lastMenstrationDayFirstDay = LocalDate.of(yearAndFirstDayOfYourLastMenstration,monthAndFirstDayOfYourLastMenstration,dayAndFirstDayOfYourLastMenstration);
+	LocalDate lastMenstrationDayLastDay = LocalDate.of(yearAndLastDayOfYourLastMenstration,monthAndLastDayOfYourLastMenstration,dayAndLastDayOfYourLastMenstration);
+	LocalDate nextPeriod = lastMenstrationDayFirstDay.plusDay(averageCycle);
+	long consLengthToGetOvulation = 14;
+	long ovulation = averageCycle - consLengthToGetOvulation;
+	LocalDate ovulationPerion = lastMenstrationDayFirstDay.plusDay(ovulation);
+	}
+}
