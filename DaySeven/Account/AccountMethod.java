8c9ac20c324710509cfa839ad16af3928be24405
Eniolaonafujabi <165755379+Eniolaonafujabi@users.noreply.github.com>
2024-05-15 07:34:42 +0100

    work

--- DaySeven/Account/AccountMethod.java
+++ DaySeven/Account/AccountMethod.java
@@ -23,21 +23,36 @@ public class AccountMethod{
 		while(newpin.length()==4);
 	}
 
+	public void changePin(){
+		System.out.println("Enter your account first name: ");
+		String accountFirstName = collector.next();
+		System.out.println("Enter your pin: ");
+		String pinInputed = collector.next();
+		for(int count = 0; count<firstName.size();count++){
+			if(firstName.get(count).equals(accountFirstName)&&pin.get(count).equals(pinInputed)){
+				do{
+					System.out.println("Create new pin Enter only 4 digit: ");
+					String newPin = collector.next();
+					if(newpin.length()==4)pin.set(count,newPin);
+				while(newpin.length()==4);
+			}
+		}
+	}
+
 	public void closeAccount(){
 		System.out.println("Enter your account first name: ");
 		String accountFirstName = collector.next();
 		System.out.println("Enter your account last name: ");
 		String accountLastName = collector.next();
-		int counter = firstName.size();
 		for(int count = 0; count<firstName.size();count){
 			if(firstName.get(count).equals(accountFirstName)){
 				System.out.println("Enter pin");
 				String pinInputed = collector.next();
 				for(int counter = 0; counter<pin.size();counter++){
-					if(pin.get(counter).equals(pinInputed)){
-						firstName.remove(counter)
-						lastName.remove(counter)
-						pin.remove(counter);	
+					if(pin.get(count).equals(pinInputed)){
+						firstName.remove(count)
+						lastName.remove(count)
+						pin.remove(count);	
 					}
 				}
 			}	
@@ -50,7 +65,7 @@ public class AccountMethod{
 		System.out.println("Enter your pin: ");
 		String pinInputed = collector.next();
 		for(int count = 0; count<firstName.size();count++){
-			if(firstName.get(count).equals(accountFirstName)&&pin.get(counter).equals(pinInputed)){
+			if(firstName.get(count).equals(accountFirstName)&&pin.get(count).equals(pinInputed)){
 				System.out.println("How much do you want to deposit: ");
 				double moneyDeposited = collector.nextDouble();
 				if(moneyDeposited>0)balance.set(counter,moneyDeposited+balance.get(count));		
@@ -64,11 +79,56 @@ public class AccountMethod{
 		System.out.println("Enter your pin: ");
 		String pinInputed = collector.next();
 		for(int count = 0; count<firstName.size();count++){
-			if(firstName.get(count).equals(accountFirstName)&&pin.get(counter).equals(pinInputed)){
+			if(firstName.get(count).equals(accountFirstName)&&pin.get(count).equals(pinInputed)){
 				System.out.println("How much do you want to withdraw: ");
 				double moneywithdraw = collector.nextDouble();
 				if(moneydraw<=balance.get(count))balance.set(counter,moneywithdraw-balance.get(count));		
 			}	
 		}		
 	}
+
+	public void checkBalance(){
+		System.out.println("Enter your account first name: ");
+		String accountFirstName = collector.next();
+		System.out.println("Enter your pin: ");
+		String pinInputed = collector.next();
+		for(int count = 0; count<firstName.size();count++){
+			if(firstName.get(count).equals(accountFirstName)&&pin.get(count).equals(pinInputed)){
+				System.out.println(balance.get(count));
+			}
+		}
+	}
+
+	public void transfer(){
+		System.out.println("Enter your account first name: ");
+		String accountFirstName = collector.next();
+		System.out.println("Enter your pin: ");
+		String pinInputed = collector.next();
+		int counter1 = firstName.size();
+		for(int count = 0; count<firstName.size();count++){
+			if(firstName.get(count).equals(accountFirstName)&&pin.get(count).equals(pinInputed)){
+				System.out.println("Enter first name of the person you want to transfer to");
+				String firstNameTransfer = collector.next();
+				System.out.println("Enter second name of the person you want to transfer to");
+				String secondNameTransfer = collector.next();
+				System.out.println("How much do you want to transfer: ");
+				double transfer = collector.nextDouble();
+				if(transfer>balance.get(count))System.out.println("Balance not enough ");
+				if(transfer<=balance.get(count)){
+					balance.set(count,balance.get(count)-transfer)
+					for(int counter = 0; counter<firstName.size();counter++){
+						if(firstName.get(counter).equals(firstNameTransfer)&&lastName.get(counter).equals(secondNameTransfer)){
+							balance.set(counter,balance.get(counter)+transfer);
+							counter1--;	
+						}
+					}
+					if(counter1<firstName.size()){
+					System.out.println("Account not found");
+					balance.set(count,balance.get(count)+transfer);
+					}
+				}		
+			}	
+		}		
+	}
+s
 }
